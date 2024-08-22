2d77ff0822e1bf1afeb10e3eb71588d20a8c9050
c-kiplimo <kiplimocollins855@gmail.com>
2024-08-22 22:09:09 +0300

    implement calculate area contract

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+require('@nomiclabs/hardhat-ethers')
+module.exports = {
+  solidity: {
+    version: "0.5.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/1b418729725246bbaee99a2ac3ac2870`,
+      accounts: [`account private key`] 
+    }
+  }
+};
